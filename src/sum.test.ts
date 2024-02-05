b8d00a15b558b30f139968e8a8168717cc748a45
marquesdanielr <82175768+marquesdanielr@users.noreply.github.com>
2024-02-05 16:27:50 -0300

    add

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -4,7 +4,7 @@ describe("math", () => {
     it('shoud be able to sum A and B', () => {
         expect(sum(2, 2)).toBe(4);
     });
-    it('shoud be able fail', () => {
-        expect(sum(2, 2)).toBe(5);
+    it('shoud be able to sum 2 and 3', () => {
+        expect(sum(2, 3)).toBe(5);
     });
 });
